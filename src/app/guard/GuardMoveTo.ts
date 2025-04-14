95537c318a3906a09d84f8309ac3dc696d05372f
ArsikMarsik <arslan.maxmutovl@gmail.com>
2025-04-14 21:15:59 +0400

    Update GuardMoveTo.ts

--- src/app/guard/GuardMoveTo.ts
+++ src/app/guard/GuardMoveTo.ts
@@ -1,5 +1,5 @@
 import { ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
 
 export const guardMoveTo = (route: ActivatedRouteSnapshot, router: RouterStateSnapshot) => {
-    return confirm("Вы точно хотите перейти на эту страницу?");
+    return confirm("Вы точно хотите уйти на другую страницу? :(");
 }
